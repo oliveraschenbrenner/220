7074ca85e6b4437fa9039d2f40983581917c4b07
oliveraschenbrenner <oliverasch@gmail.com>
2021-09-01 13:18:50 -0400

    completed hw1 - printing hello, world!

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,16 @@
+"""
+Name: Oliver Aschenbrenner
+hello.py
+
+Problem: print hello, world! to the screen
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
